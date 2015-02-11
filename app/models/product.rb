e3852db6df2ed07971790043c5846f3ac6cc6ef3
tokinonagare <timemahou@gmail.com>
2015-02-11 17:08:43 +0800

    Add Catalog Display

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+
+	def self.latest
+		Product.order(:updated_at).last
+	end
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
