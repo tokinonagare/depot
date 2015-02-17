85e027fe4b48b5b18a24fac0591a9ac67e7f7e6b
tokinonagare <timemahou@gmail.com>
2015-02-17 15:24:28 +0800

    Add order & Atom Feed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
@@ -58,6 +59,26 @@ class ProductsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
+    end
+  end
+
+  # def who_bought
+  #   @product = Product.find(params[:id])
+  #   @latest_order = @product.orders.order(:update_at).last
+  #   if stale?(@latest_order)
+  #     respond_to do |format|
+  #       format.atom
+  #     end
+  #   end
+  # end
+
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+      if stale?(@latest_order)
+        respond_to do |format| 
+          format.atom
+      end 
     end
   end
 
